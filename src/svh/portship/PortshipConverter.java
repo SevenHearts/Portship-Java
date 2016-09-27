9f841cea9180c05fe8a72cd8a9af8846e9b795bd
Josh Junon <i.am.qix@gmail.com>
2016-09-26 22:00:24 -0700

    add HIM to JSON dumping

--- src/svh/portship/PortshipConverter.java
+++ src/svh/portship/PortshipConverter.java
@@ -4,17 +4,23 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+
+import com.google.gson.Gson;
 
 import magick.ImageInfo;
 import magick.MagickException;
 import magick.MagickImage;
+import svh.portship.format.HIMFile;
 import svh.portship.format.vfs.VFSFile;
 import svh.portship.util.IOUtil;
 
 public final class PortshipConverter {
 
+	private static final Gson GSON = new Gson();
+	
 	private PortshipConverter() {
 	}
 	
@@ -25,22 +31,40 @@ public final class PortshipConverter {
 		}
 	}
 
-	public static boolean convert(final VFSFile vfsFile, File outFile, InputStream stream,
-			String progress) throws UnknownTypeException {
+	public static boolean convert(final VFSFile vfsFile, File outFile, InputStream stream, String progress) throws UnknownTypeException {
 		String[] exts = outFile.getName().split("\\.");
 		if (exts.length <= 1) {
 			return false;
 		}
+
 		String ext = exts[exts.length - 1];
+		String original = outFile.toString();
 
 		switch (ext) {
 		case "dds":
-			String original = outFile.toString();
 			outFile = PortshipConverter.changeExt(outFile, "png");
 			return PortshipConverter.toPNG(vfsFile, stream, original, outFile, progress);
+		case "him":
+			outFile = PortshipConverter.changeExt(outFile, "him.json");
+			return PortshipConverter.toHeightJSON(stream, outFile, progress);
 		default:
 			throw new UnknownTypeException();
 		}
+	}
+
+	private static boolean toHeightJSON(InputStream stream, File outFile, String progress) {
+		try {
+			HIMFile him = new HIMFile(stream);
+			Portship.LOG.info(progress + "\u001b[38;5;208mHIM -> JSON:\u001b[m " + outFile.toString());
+			try (FileWriter fw = new FileWriter(outFile)) {
+				fw.write(PortshipConverter.GSON.toJson(him));
+			}
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+			return false;
+		}
+		
+		return true;
 	}
 
 	private static boolean toPNG(VFSFile vfsFile, InputStream ddsStream, String originalName, File newFile, String progress) {
@@ -67,7 +91,7 @@ public final class PortshipConverter {
 					IOUtil.pipe(bis, fos);
 				}
 			}
-			Portship.LOG.info(progress + "\u001b[38;5;199mDDS -> PNG:\u001b[0m " + newFile.toString());
+			Portship.LOG.info(progress + "\u001b[38;5;199mDDS -> PNG:\u001b[m " + newFile.toString());
 		} catch (MagickException | IOException e) {
 			e.printStackTrace(System.err);
 			return false;
@@ -75,7 +99,7 @@ public final class PortshipConverter {
 
 		return true;
 	}
-
+	
 	private static File changeExt(File original, String extension) {
 		String fs = original.toString();
 		return new File(fs.substring(0, fs.lastIndexOf('.')) + "." + extension);
--- src/svh/portship/format/HIMFile.java
+++ src/svh/portship/format/HIMFile.java
@@ -1,15 +1,16 @@
 package svh.portship.format;
 
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+
+import svh.portship.util.LittleEndianDataInputStream;
 
 /**
  * Height map information
  */
 public class HIMFile {
-	private final File file;
 	private final int length;
 	private final int width;
 	private final int gridCount;
@@ -17,17 +18,26 @@ public class HIMFile {
 	private final float[][] heights;
 	private final float minHeight;
 	private final float maxHeight;
-
+	
+	@SuppressWarnings("resource")
 	public HIMFile(File file) throws IOException {
-		this.file = file;
-
-		try (FileInputStream fis = new FileInputStream(this.file)) {
-			try (DataInputStream dis = new DataInputStream(fis)) {
+		this(new FileInputStream(file), true);
+	}
+	
+	public HIMFile(InputStream stream) throws IOException {
+		this(stream, false);
+	}
+	
+	public HIMFile(InputStream stream, boolean autoClose) throws IOException {
+		try {
+			try (LittleEndianDataInputStream dis = new LittleEndianDataInputStream(stream)) {
 				this.length = dis.readInt();
 				this.width = dis.readInt();
 				this.gridCount = dis.readInt();
 				this.gridSize = dis.readFloat();
 
+				System.out.format("length=%d width=%d gridCount=%d gridSize=%f%n", this.length, this.width, this.gridCount, this.gridSize);
+		
 				float min = Float.POSITIVE_INFINITY;
 				float max = Float.NEGATIVE_INFINITY;
 				float current = 0f;
@@ -36,52 +46,52 @@ public class HIMFile {
 					this.heights[y] = new float[this.width];
 					for (int x = 0; x < this.width; x++) {
 						this.heights[y][x] = current = dis.readFloat();
-
+		
 						if (current < min) {
 							min = current;
 						}
-
+		
 						if (current > max) {
 							max = current;
 						}
 					}
 				}
-
+		
 				this.minHeight = min;
 				this.maxHeight = max;
+			}
+		} finally {
+			if (autoClose) {
+				stream.close();
 			}
 		}
 	}
 
-	private File getFile() {
-		return this.file;
-	}
-
-	private int getLength() {
+	public int getLength() {
 		return this.length;
 	}
 
-	private int getWidth() {
+	public int getWidth() {
 		return this.width;
 	}
 
-	private int getGridCount() {
+	public int getGridCount() {
 		return this.gridCount;
 	}
 
-	private float getGridSize() {
+	public float getGridSize() {
 		return this.gridSize;
 	}
 
-	private float[][] getHeights() {
+	public float[][] getHeights() {
 		return this.heights;
 	}
 
-	private float getMinHeight() {
+	public float getMinHeight() {
 		return this.minHeight;
 	}
 
-	private float getMaxHeight() {
+	public float getMaxHeight() {
 		return this.maxHeight;
 	}
 }
